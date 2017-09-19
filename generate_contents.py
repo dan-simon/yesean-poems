0e1ecf0db6dca59be88fddebc403b11866bc731d
dan-simon <dan@pipeline.com>
2017-09-18 22:00:06 -0400

    second commit

---
+++ generate_contents.py
@@ -0,0 +1,73 @@
+import os
+import re
+
+entry_pattern = r'\d+\.html'
+
+start = '''<html>
+<head>
+    <meta charset='UTF-8'>
+</head>
+<body>
+    List of entries, together with their tags:
+    <br/>
+    '''
+
+middle = '''
+    <br/>
+    List of tags, together with entries with that tag:
+    <br/>
+    '''
+
+end = '''
+</body>
+</html>
+'''
+
+def sort_files(l):
+    return list(sorted(l, key=lambda x: int(x.split('.')[0])))
+
+def join_br(l):
+    return '\n    <br/>\n    '.join(l)
+
+def valid_tag(tag):
+    return tag[0] == '@' and tag[1:].isalpha() and tag[1:].islower()
+
+def get_tags(x):
+    tag_line_start = 'Tags:'
+    with open(x) as f:
+        tag_lines = [l for l in f.readlines() if l.strip().startswith(tag_line_start)]
+    if len(tag_lines) != 1:
+        raise ValueError('Too few or too many tag lines (' + str(len(tag_lines)) + ', not 1).')
+    tags = [i.strip() for i in tag_lines[0].strip()[len(tag_line_start):].strip().split(',')]
+    if any(not valid_tag(tag) for tag in tags):
+        raise ValueError('Bad tags: ' + ', '.join(tag for tag in tags if not valid_tag(tag)))
+    return tags
+
+def link_file(f):
+    return '<a href="' + f + '">' + f + '</a>'
+
+def get_tag_dict(files, tags):
+    d = {}
+    for f, t in zip(files, tags):
+        for tag in t:
+            d.setdefault(tag, [])
+            d[tag].append(f)
+    return d
+
+def main_and_tags():
+    files = sort_files(i for i in os.listdir('.') if re.match(entry_pattern, i))
+    tags = [list(sorted(get_tags(i))) for i in files]
+    main_section = [link_file(f) + ', with tags ' + ', '.join(t) for f, t in zip(files, tags)]
+    tag_dict = get_tag_dict(files, tags)
+    tags_section = ['Tag ' + t + ': ' + ', '.join(link_file(i) for i in tag_dict[t])
+    for t in sorted(tag_dict.keys())]
+    return join_br(main_section), join_br(tags_section)
+
+def main():
+    main_section, tags_section = main_and_tags()
+    text = start + main_section + middle + tags_section + end
+    with open('contents.html', 'w') as f:
+        f.write(text)
+
+if __name__ == '__main__':
+    main()
